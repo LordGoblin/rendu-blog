1343cbeba3210b970dd9f1c218a894073920ae37
LordGoblin <maxime78600@gmail.com>
2019-01-30 17:32:55 +0100

    ok

---
+++ db/migrate/20190130160147_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.belongs_to :user, index: true
+      t.string :title
+      t.string :content
+      t.belongs_to :categorie, index: true
+
+      t.timestamps
+    end
+  end
+end
